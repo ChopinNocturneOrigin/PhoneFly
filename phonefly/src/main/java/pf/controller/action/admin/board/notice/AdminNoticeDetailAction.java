19ccdf702239dc0f822d586ee8932f35d6185b2d
Ming goo <130649685+llkkmm0823@users.noreply.github.com>
2023-05-25 12:15:46 +0900

    오전작업

--- phonefly/src/main/java/pf/controller/action/admin/board/notice/AdminNoticeDetailAction.java
+++ phonefly/src/main/java/pf/controller/action/admin/board/notice/AdminNoticeDetailAction.java
@@ -5,14 +5,29 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import pf.controller.action.Action;
+import pf.dao.NoticeDao;
+import pf.dto.AdminVO;
+import pf.dto.NoticeVO;
 
 public class AdminNoticeDetailAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		String url = "admin/notice/adminNoticeDetail.jsp";
+		HttpSession session = request.getSession();
+		AdminVO avo = (AdminVO)session.getAttribute("loginAdmin");
+		if( avo == null) { 
+			url = "pf.do?command=admin"; 
+		} else {		
+			NoticeDao ndao = NoticeDao.getInstance();
+			NoticeVO nvo = ndao.getNotice(Integer.parseInt(request.getParameter("nseq")));
+			
+			request.setAttribute("NoticeVO", nvo);
+		}
+		request.getRequestDispatcher(url).forward(request, response);
 
 	}
 
--- phonefly/src/main/java/pf/controller/action/admin/product/AdminColorDeleteAction.java
+++ phonefly/src/main/java/pf/controller/action/admin/product/AdminColorDeleteAction.java
@@ -5,15 +5,30 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import pf.controller.action.Action;
+import pf.dao.ColorDao;
+import pf.dto.AdminVO;
+import pf.dto.MemberVO;
 
 public class AdminColorDeleteAction implements Action {
 
 	@Override
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+		int cseq=Integer.parseInt(request.getParameter("cseq"));
+		
+		String url="admin/product/adminColorList.jsp";
+		
+		HttpSession session = request.getSession();
+		AdminVO avo = (AdminVO) session.getAttribute("loginAdmin");
+		if(avo==null) {
+			url="pf.do?command=admin";
+		}else {
+			ColorDao cdao = ColorDao.getInstance();
+			cdao.deleteColor(cseq);
+		}
+		request.getRequestDispatcher(url).forward(request, response);
 	}
 
 }
